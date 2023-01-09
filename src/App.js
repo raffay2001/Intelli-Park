0abe726896eb8c27e89f50a5976f01dbb21443a6
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-01-09 18:54:10 +0500

    add: setted up tailwind and pathfinding libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Grid, AStarFinder } from "pathfinding"
+
+const findPath = () => {
+  let grid = new Grid(10, 10)
+  let finder = new AStarFinder()
+  let path = finder.findPath(2, 3, 4, 4, grid)
+  console.log(path)
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Intelli Park</h1>
+    <div className="flex h-screen justify-center text-zinc-900">
+      <h1 className="font-extrabold m-0 p-0 pt-5 text-5xl">Intelli Park</h1>
+      {findPath()}
     </div>
   )
 }
