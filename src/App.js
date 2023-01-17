864a97ce3bf9f6bc5898930fb799df0b65498413
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-01-17 12:53:17 +0500

    add: highlighted paths for start, goal and final route cells

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import { Grid, AStarFinder } from "pathfinding"
 import { useState } from "react"
-import { useDrag } from "react-use-gesture"
 
 function App() {
+  // reactive varible for storing nodes.
   const [nodes, setNodes] = useState([])
+  const [startNode, setStartNode] = useState([])
+  const [goalNode, setGoaltNode] = useState([])
+  const [path, setPath] = useState([])
 
   const findPath = () => {
-    let grid = new Grid(10, 10)
+    let grid = new Grid(5, 5)
     let finder = new AStarFinder()
     const [startNode, goalNode] = nodes
-    let path = finder.findPath(startNode[0], startNode[1], goalNode[0], goalNode[1], grid)
-    alert(`The path is: ${JSON.stringify(path)}`)
+    let finalPath = finder.findPath(startNode[0], startNode[1], goalNode[0], goalNode[1], grid)
+    setPath(finalPath)
+    alert(`The path is: ${JSON.stringify(finalPath)}`)
     // [[0,2],[0,3],[0,4]]
   }
 
   const nodeHandler = (_, rows, cols) => {
-    nodes.length < 2 ? nodes.push([rows, cols]) : alert("Cannot push!!!")
+    // nodes.length < 2 ? nodes.push([rows, cols]) : alert("Cannot push!!!")
+    if (nodes.length < 2) {
+      nodes.length === 0 && setStartNode([rows, cols])
+      nodes.length === 1 && setGoaltNode([rows, cols])
+      nodes.push([rows, cols])
+    } else {
+      alert("Cannot push")
+    }
   }
 
   return (
@@ -34,7 +45,7 @@ function App() {
           See Path
         </button>
       </div>
-      <div className="my-2 bg-slate-600 text-white">
+      <div className="my-2 pb-5">
         {Array(5)
           .fill("0")
           .map((_, rows) => {
@@ -44,7 +55,14 @@ function App() {
                   .fill("0")
                   .map((_, cols) => (
                     <div
-                      className={`w-20 h-20 border border-solid border-white`}
+                      className={`w-20 h-20 border border-solid border-zinc-900 ${
+                        startNode[0] === rows && startNode[1] === cols ? "bg-gray-600" : ""
+                      } ${goalNode[0] === rows && goalNode[1] === cols ? "bg-gray-600" : ""} ${path
+                        .slice(1, path.length - 1)
+                        .map((node) =>
+                          node[0] === rows && node[1] === cols ? "bg-emerald-900" : ""
+                        )
+                        .join("")}`}
                       onClick={(e, i = rows, j = cols) => nodeHandler(e, i, j)}
                     >
                       ({`${rows},${cols}`})
