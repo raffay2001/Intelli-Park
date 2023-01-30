6a1f492b1d648096c2457e0669fb7395f1db70e8
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-01-30 21:26:43 +0500

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,466 @@ import Select from "@mui/material/Select"
 import ElectricCarIcon from "@mui/icons-material/ElectricCar"
 import toast, { Toaster } from "react-hot-toast"
 
+// Implemented Algorithms
+
+// 1- A-star:
+var Heap = require("heap")
+var Util = require("../core/Util")
+var Heuristic = require("../core/Heuristic")
+var DiagonalMovement = require("../core/DiagonalMovement")
+
+/**
+ * A* path-finder. Based upon https://github.com/bgrins/javascript-astar
+ * @constructor
+ * @param {Object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
+ *     Deprecated, use diagonalMovement instead.
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
+ *     block corners. Deprecated, use diagonalMovement instead.
+ * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ * @param {number} opt.weight Weight to apply to the heuristic to allow for
+ *     suboptimal paths, in order to speed up the search.
+ */
+function AStarFinder(opt) {
+  opt = opt || {}
+  this.allowDiagonal = opt.allowDiagonal
+  this.dontCrossCorners = opt.dontCrossCorners
+  this.heuristic = opt.heuristic || Heuristic.manhattan
+  this.weight = opt.weight || 1
+  this.diagonalMovement = opt.diagonalMovement
+
+  if (!this.diagonalMovement) {
+    if (!this.allowDiagonal) {
+      this.diagonalMovement = DiagonalMovement.Never
+    } else {
+      if (this.dontCrossCorners) {
+        this.diagonalMovement = DiagonalMovement.OnlyWhenNoObstacles
+      } else {
+        this.diagonalMovement = DiagonalMovement.IfAtMostOneObstacle
+      }
+    }
+  }
+
+  // When diagonal movement is allowed the manhattan heuristic is not
+  //admissible. It should be octile instead
+  if (this.diagonalMovement === DiagonalMovement.Never) {
+    this.heuristic = opt.heuristic || Heuristic.manhattan
+  } else {
+    this.heuristic = opt.heuristic || Heuristic.octile
+  }
+}
+
+/**
+ * Find and return the the path.
+ * @return {Array<Array<number>>} The path, including both start and
+ *     end positions.
+ */
+AStarFinder.prototype.findPath = function (startX, startY, endX, endY, grid) {
+  var openList = new Heap(function (nodeA, nodeB) {
+      return nodeA.f - nodeB.f
+    }),
+    startNode = grid.getNodeAt(startX, startY),
+    endNode = grid.getNodeAt(endX, endY),
+    heuristic = this.heuristic,
+    diagonalMovement = this.diagonalMovement,
+    weight = this.weight,
+    abs = Math.abs,
+    SQRT2 = Math.SQRT2,
+    node,
+    neighbors,
+    neighbor,
+    i,
+    l,
+    x,
+    y,
+    ng
+
+  // set the `g` and `f` value of the start node to be 0
+  startNode.g = 0
+  startNode.f = 0
+
+  // push the start node into the open list
+  openList.push(startNode)
+  startNode.opened = true
+
+  // while the open list is not empty
+  while (!openList.empty()) {
+    // pop the position of node which has the minimum `f` value.
+    node = openList.pop()
+    node.closed = true
+
+    // if reached the end position, construct the path and return it
+    if (node === endNode) {
+      return Util.backtrace(endNode)
+    }
+
+    // get neigbours of the current node
+    neighbors = grid.getNeighbors(node, diagonalMovement)
+    for (i = 0, l = neighbors.length; i < l; ++i) {
+      neighbor = neighbors[i]
+
+      if (neighbor.closed) {
+        continue
+      }
+
+      x = neighbor.x
+      y = neighbor.y
+
+      // get the distance between current node and the neighbor
+      // and calculate the next g score
+      ng = node.g + (x - node.x === 0 || y - node.y === 0 ? 1 : SQRT2)
+
+      // check if the neighbor has not been inspected yet, or
+      // can be reached with smaller cost from the current node
+      if (!neighbor.opened || ng < neighbor.g) {
+        neighbor.g = ng
+        neighbor.h = neighbor.h || weight * heuristic(abs(x - endX), abs(y - endY))
+        neighbor.f = neighbor.g + neighbor.h
+        neighbor.parent = node
+
+        if (!neighbor.opened) {
+          openList.push(neighbor)
+          neighbor.opened = true
+        } else {
+          // the neighbor can be reached with smaller cost.
+          // Since its f value has been updated, we have to
+          // update its position in the open list
+          openList.updateItem(neighbor)
+        }
+      }
+    } // end for each neighbor
+  } // end while not open list empty
+
+  // fail to find the path
+  return []
+}
+
+module.exports = AStarFinder
+
+// 2- Best First Algorithm
+var AStarFinder = require("./AStarFinder")
+
+/**
+ * Best-First-Search path-finder.
+ * @constructor
+ * @extends AStarFinder
+ * @param {Object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
+ *     Deprecated, use diagonalMovement instead.
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
+ *     block corners. Deprecated, use diagonalMovement instead.
+ * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ */
+function BestFirstFinder(opt) {
+  AStarFinder.call(this, opt)
+
+  var orig = this.heuristic
+  this.heuristic = function (dx, dy) {
+    return orig(dx, dy) * 1000000
+  }
+}
+
+BestFirstFinder.prototype = new AStarFinder()
+BestFirstFinder.prototype.constructor = BestFirstFinder
+
+module.exports = BestFirstFinder
+
+// 3- BFS:
+var Util = require("../core/Util")
+var DiagonalMovement = require("../core/DiagonalMovement")
+
+/**
+ * Breadth-First-Search path finder.
+ * @constructor
+ * @param {Object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
+ *     Deprecated, use diagonalMovement instead.
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
+ *     block corners. Deprecated, use diagonalMovement instead.
+ * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
+ */
+function BreadthFirstFinder(opt) {
+  opt = opt || {}
+  this.allowDiagonal = opt.allowDiagonal
+  this.dontCrossCorners = opt.dontCrossCorners
+  this.diagonalMovement = opt.diagonalMovement
+
+  if (!this.diagonalMovement) {
+    if (!this.allowDiagonal) {
+      this.diagonalMovement = DiagonalMovement.Never
+    } else {
+      if (this.dontCrossCorners) {
+        this.diagonalMovement = DiagonalMovement.OnlyWhenNoObstacles
+      } else {
+        this.diagonalMovement = DiagonalMovement.IfAtMostOneObstacle
+      }
+    }
+  }
+}
+
+/**
+ * Find and return the the path.
+ * @return {Array<Array<number>>} The path, including both start and
+ *     end positions.
+ */
+BreadthFirstFinder.prototype.findPath = function (startX, startY, endX, endY, grid) {
+  var openList = [],
+    diagonalMovement = this.diagonalMovement,
+    startNode = grid.getNodeAt(startX, startY),
+    endNode = grid.getNodeAt(endX, endY),
+    neighbors,
+    neighbor,
+    node,
+    i,
+    l
+
+  // push the start pos into the queue
+  openList.push(startNode)
+  startNode.opened = true
+
+  // while the queue is not empty
+  while (openList.length) {
+    // take the front node from the queue
+    node = openList.shift()
+    node.closed = true
+
+    // reached the end position
+    if (node === endNode) {
+      return Util.backtrace(endNode)
+    }
+
+    neighbors = grid.getNeighbors(node, diagonalMovement)
+    for (i = 0, l = neighbors.length; i < l; ++i) {
+      neighbor = neighbors[i]
+
+      // skip this neighbor if it has been inspected before
+      if (neighbor.closed || neighbor.opened) {
+        continue
+      }
+
+      openList.push(neighbor)
+      neighbor.opened = true
+      neighbor.parent = node
+    }
+  }
+
+  // fail to find the path
+  return []
+}
+
+module.exports = BreadthFirstFinder
+
+// 4- IDS
+var Util = require("../core/Util")
+var Heuristic = require("../core/Heuristic")
+var Node = require("../core/Node")
+var DiagonalMovement = require("../core/DiagonalMovement")
+
+/**
+ * Iterative Deeping A Star (IDA*) path-finder.
+ *
+ * Recursion based on:
+ *   http://www.apl.jhu.edu/~hall/AI-Programming/IDA-Star.html
+ *
+ * Path retracing based on:
+ *  V. Nageshwara Rao, Vipin Kumar and K. Ramesh
+ *  "A Parallel Implementation of Iterative-Deeping-A*", January 1987.
+ *  ftp://ftp.cs.utexas.edu/.snapshot/hourly.1/pub/AI-Lab/tech-reports/UT-AI-TR-87-46.pdf
+ *
+ * @author Gerard Meier (www.gerardmeier.com)
+ *
+ * @constructor
+ * @param {Object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
+ *     Deprecated, use diagonalMovement instead.
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
+ *     block corners. Deprecated, use diagonalMovement instead.
+ * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ * @param {number} opt.weight Weight to apply to the heuristic to allow for
+ *     suboptimal paths, in order to speed up the search.
+ * @param {boolean} opt.trackRecursion Whether to track recursion for
+ *     statistical purposes.
+ * @param {number} opt.timeLimit Maximum execution time. Use <= 0 for infinite.
+ */
+function IDAStarFinder(opt) {
+  opt = opt || {}
+  this.allowDiagonal = opt.allowDiagonal
+  this.dontCrossCorners = opt.dontCrossCorners
+  this.diagonalMovement = opt.diagonalMovement
+  this.heuristic = opt.heuristic || Heuristic.manhattan
+  this.weight = opt.weight || 1
+  this.trackRecursion = opt.trackRecursion || false
+  this.timeLimit = opt.timeLimit || Infinity // Default: no time limit.
+
+  if (!this.diagonalMovement) {
+    if (!this.allowDiagonal) {
+      this.diagonalMovement = DiagonalMovement.Never
+    } else {
+      if (this.dontCrossCorners) {
+        this.diagonalMovement = DiagonalMovement.OnlyWhenNoObstacles
+      } else {
+        this.diagonalMovement = DiagonalMovement.IfAtMostOneObstacle
+      }
+    }
+  }
+
+  // When diagonal movement is allowed the manhattan heuristic is not
+  // admissible, it should be octile instead
+  if (this.diagonalMovement === DiagonalMovement.Never) {
+    this.heuristic = opt.heuristic || Heuristic.manhattan
+  } else {
+    this.heuristic = opt.heuristic || Heuristic.octile
+  }
+}
+
+/**
+ * Find and return the the path. When an empty array is returned, either
+ * no path is possible, or the maximum execution time is reached.
+ *
+ * @return {Array<Array<number>>} The path, including both start and
+ *     end positions.
+ */
+IDAStarFinder.prototype.findPath = function (startX, startY, endX, endY, grid) {
+  // Used for statistics:
+  var nodesVisited = 0
+
+  // Execution time limitation:
+  var startTime = new Date().getTime()
+
+  // Heuristic helper:
+  var h = function (a, b) {
+    return this.heuristic(Math.abs(b.x - a.x), Math.abs(b.y - a.y))
+  }.bind(this)
+
+  // Step cost from a to b:
+  var cost = function (a, b) {
+    return a.x === b.x || a.y === b.y ? 1 : Math.SQRT2
+  }
+
+  /**
+   * IDA* search implementation.
+   *
+   * @param {Node} The node currently expanding from.
+   * @param {number} Cost to reach the given node.
+   * @param {number} Maximum search depth (cut-off value).
+   * @param {Array<Array<number>>} The found route.
+   * @param {number} Recursion depth.
+   *
+   * @return {Object} either a number with the new optimal cut-off depth,
+   * or a valid node instance, in which case a path was found.
+   */
+  var search = function (node, g, cutoff, route, depth) {
+    nodesVisited++
+
+    // Enforce timelimit:
+    if (this.timeLimit > 0 && new Date().getTime() - startTime > this.timeLimit * 1000) {
+      // Enforced as "path-not-found".
+      return Infinity
+    }
+
+    var f = g + h(node, end) * this.weight
+
+    // We've searched too deep for this iteration.
+    if (f > cutoff) {
+      return f
+    }
+
+    if (node == end) {
+      route[depth] = [node.x, node.y]
+      return node
+    }
+
+    var min, t, k, neighbour
+
+    var neighbours = grid.getNeighbors(node, this.diagonalMovement)
+
+    // Sort the neighbours, gives nicer paths. But, this deviates
+    // from the original algorithm - so I left it out.
+    //neighbours.sort(function(a, b){
+    //    return h(a, end) - h(b, end);
+    //});
+
+    /*jshint -W084 */ //Disable warning: Expected a conditional expression and instead saw an assignment
+    for (k = 0, min = Infinity; (neighbour = neighbours[k]); ++k) {
+      /*jshint +W084 */ //Enable warning: Expected a conditional expression and instead saw an assignment
+      if (this.trackRecursion) {
+        // Retain a copy for visualisation. Due to recursion, this
+        // node may be part of other paths too.
+        neighbour.retainCount = neighbour.retainCount + 1 || 1
+
+        if (neighbour.tested !== true) {
+          neighbour.tested = true
+        }
+      }
+
+      t = search(neighbour, g + cost(node, neighbour), cutoff, route, depth + 1)
+
+      if (t instanceof Node) {
+        route[depth] = [node.x, node.y]
+
+        // For a typical A* linked list, this would work:
+        // neighbour.parent = node;
+        return t
+      }
+
+      // Decrement count, then determine whether it's actually closed.
+      if (this.trackRecursion && --neighbour.retainCount === 0) {
+        neighbour.tested = false
+      }
+
+      if (t < min) {
+        min = t
+      }
+    }
+
+    return min
+  }.bind(this)
+
+  // Node instance lookups:
+  var start = grid.getNodeAt(startX, startY)
+  var end = grid.getNodeAt(endX, endY)
+
+  // Initial search depth, given the typical heuristic contraints,
+  // there should be no cheaper route possible.
+  var cutOff = h(start, end)
+
+  var j, route, t
+
+  // With an overflow protection.
+  for (j = 0; true; ++j) {
+    route = []
+
+    // Search till cut-off depth:
+    t = search(start, 0, cutOff, route, 0)
+
+    // Route not possible, or not found in time limit.
+    if (t === Infinity) {
+      return []
+    }
+
+    // If t is a node, it's also the end node. Route is now
+    // populated with a valid path to the end node.
+    if (t instanceof Node) {
+      return route
+    }
+
+    // Try again, this time with a deeper cut-off. The t score
+    // is the closest we got to the end node.
+    cutOff = t
+  }
+
+  // This _should_ never to be reached.
+  return []
+}
+
+module.exports = IDAStarFinder
+
 // Helper function to check if the element exists in the array or not
 const isArrayItemExists = (arr, item) => {
   for (let i = 0; i < arr.length; i++) {
